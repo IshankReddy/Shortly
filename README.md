e94b4ad4fab7df281a27ca03947376c589608cb5
Ishank <ishank@Ishanks-MacBook-Air.local>
2024-11-25 09:07:29 +0800

    commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Shortly
+
+This is a website that shorten links it has vaildation but sadly it dosen't shorten links but can try make it work.
+
+# How to run?
+
+Delete node_modules
+
+Run npm install
+
+Run the HTML file
+
+# How to edit?
+
+Run npm run watch
---
+++ js/script.js
@@ -0,0 +1,69 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+btn.addEventListener('click', navToggle)
+
+linkForm.addEventListener('submit', formSubmit)
+
+// Toggle Mobile Menu
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('hidden')
+    menu.classList.toggle('flex')
+}
+
+// Validate a URL
+function validURL(str) {
+    
+    var pattern = new RegExp(
+
+        '^(https?:\\/\\/)?' + // protocol
+
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+
+        '(\\#[-a-z\\d_]*)?$',
+
+        'i'
+
+    )
+    return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+
+    e.preventDefault()
+
+    if (input.value === '') {
+
+        errMsg.innerHTML = 'Please enter something'
+
+        input.classList.add('border-red-500')
+
+    } else if (!validURL(input.value)) {
+
+        errMsg.innerHTML = 'Please enter a valid URL'
+
+        input.classList.add('border-red-500')
+        
+    } else {
+
+        errMsg.innerHTML = ''
+
+        input.classList.remove('border-red-500')
+
+        alert('Success')
+
+    }
+
+}
+
